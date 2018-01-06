eeec8a21ff7f71962e596800b88d4c768f990ca5
Kriti Garg <kritigarg1998@gmail.com>
2018-01-06 21:42:42 +0530

    eventhandler

---
+++ app/src/main/java/com/kriti/smartbutton/MainActivity.java
@@ -0,0 +1,72 @@
+package com.kriti.smartbutton;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        Button kritisbutton = (Button)findViewById(R.id.kritisbutton);
+
+        kritisbutton.setOnClickListener(
+
+                new Button.OnClickListener() {
+                    public void onClick(View v){
+
+                    TextView kritisText = (TextView) findViewById(R.id.kritistext);
+                            kritisText.setText("good job");
+                }
+        }
+    );
+        kritisbutton.setOnLongClickListener(
+                new Button.OnLongClickListener(){
+                    public boolean onLongClick(View v){
+                        TextView kritisText = (TextView) findViewById(R.id.kritistext);
+                        kritisText.setText("yeah,a long one");
+                        return true;
+                    }
+                }
+        );
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
